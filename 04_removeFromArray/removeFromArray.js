3ac14dfaede826bf6704e630b9dc647ccdbdb121
Javier Arce <jasearce@espol.edu.ec>
2025-04-03 10:56:13 -0500

    completed removeFromArray exercise with use of rest parameters and array built-in functions (push, includes)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray, ...itemsToRemove) {
+    resultArr = [];
+    for (let i = 0; i < givenArray.length; i++) {
+        if (!itemsToRemove.includes(givenArray[i])) {
+            resultArr.push(givenArray[i]);
+        }
+    }
+    return resultArr;
 };
 
 // Do not edit below this line
