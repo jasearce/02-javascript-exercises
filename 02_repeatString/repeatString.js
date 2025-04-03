5437dbcf48c0f6e26c4376ed3bbc9dc2d07c9704
Javier Arce <jasearce@espol.edu.ec>
2025-04-03 09:14:56 -0500

    completed repeatString function with all test passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, times) {
+    let repeatedStr = "";
+    if(times < 0) return "ERROR";
+    for (let index = 0; index < times; index++) {
+        repeatedStr += phrase;
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
