c8b4a477eb0417621aa3d23fd9551e6e26aaf583
Javier Arce <jasearce@espol.edu.ec>
2025-04-03 11:49:34 -0500

    finished leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divisibleByFour = year % 4 == 0;
+    const divisibleByHundred = year % 100 === 0;
+    const divisibleByFourHundred = year % 400 === 0;
+    
+    let isLeapYear = false;
 
+    if (divisibleByFour){
+        if (!divisibleByHundred || divisibleByFourHundred) return !isLeapYear;
+    }
+    return isLeapYear;
 };
 
 // Do not edit below this line
