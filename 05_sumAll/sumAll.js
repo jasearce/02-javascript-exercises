a7293802829e72536cf96ef5e2894d430c610308
Javier Arce <jasearce@espol.edu.ec>
2025-04-03 11:19:39 -0500

    finished sumAll exercise with validations

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 ) return "ERROR";
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR"; // no floating numbers allowed
+    if (start === isNaN || end === isNaN) return "ERROR"; // no other type of data allowed (can be omitted)
+    let resultSum = 0;
+    // Works when start is greater than end
+    if (start > end) {
+        for (let i = end; i < start + 1; i++) {
+            resultSum+= i;
+        }
+    }
+    else {
+        for (let i = start; i < end + 1; i++) {
+            resultSum+= i;
+        }
+    }
+    return resultSum;
 };
 
 // Do not edit below this line
