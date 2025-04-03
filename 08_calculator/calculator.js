20e0d9ae868270cf3c091f08b62629169be767a6
Javier Arce <jasearce@espol.edu.ec>
2025-04-03 12:54:31 -0500

    completed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(...args) {
+  let result = 0;
+	args.forEach(arg => result+= arg);
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	let result = 0;
+  for (let i = 0; i < args.length; i++) {
+    if ( i === 0 ){
+      result = args[0];
+    } 
+    else {
+      result -= args[i];
+    }
+  }
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(numberArr) {
+	return numberArr.reduce((total, number) => total + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numberArr) {
+  return numberArr.reduce((total, number) => total * number);
 };
 
-const power = function() {
-	
+const power = function(...args) {
+	return args.reduce((total, number) => Math.pow(total,number));
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = 1;
+  for (let i = number; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
