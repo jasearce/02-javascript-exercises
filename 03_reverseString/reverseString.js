6c5beba3fdf8ba81612d4737b851ddd525eec109
Javier Arce <jasearce@espol.edu.ec>
2025-04-03 09:42:52 -0500

    completed reverseString exercise using String built-in functions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversedStr = "";
+    for (let i = 0; i < word.length; i++) {
+        reversedStr = reversedStr.concat(word.charAt(word.length - i - 1));
+    }
+    return reversedStr;
 };
 
 // Do not edit below this line
