2794633268ac4eee49397d6f1a12697c73c61333
Javier Arce <jasearce@espol.edu.ec>
2025-04-06 18:53:41 -0500

    complete palindromes exercises with array functions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const allowedChars = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const tmpChars = word.toLowerCase().split('').filter(letter => allowedChars.includes(letter));
+    const cleanedStr = tmpChars.join('');
+    const reversedWord = tmpChars.reverse().join('');
+    console.log(`given word: ${cleanedStr} | palindrome: ${reversedWord}`);
+    return cleanedStr === reversedWord;
 };
 
 // Do not edit below this line
