ce198c24a7118d94feee8ac197c4b12462a8b575
Javier Arce <jasearce@espol.edu.ec>
2025-04-06 19:28:03 -0500

    complete getTheTitles exercise with basic logic

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    //Version 1
+    let booksTitle = [];
+    for (let i = 0; i < books.length; i++) {
+        booksTitle.push(books[i].title);
+    }
+    return booksTitle;
 };
 
 // Do not edit below this line
