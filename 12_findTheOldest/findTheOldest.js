b040c3ab0d7b0bba261d2ac87278be836a333ea1
Javier Arce <jasearce@espol.edu.ec>
2025-04-06 20:11:08 -0500

    complete findOldest exercise with reduce() function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+function getAge(dateOfBirth, dateOfDeath) {
+    let age = 0;
+    if ( !dateOfDeath ) {
+        age = new Date().getFullYear() - dateOfBirth;
+    }
+    else {
+        age = dateOfDeath - dateOfBirth;
+    }
+    return age;
+}
 
+
+const findTheOldest = function(people) {
+    const oldestPerson = people.reduce((oldest, person) => {
+        const personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        if (oldestAge < personAge) {
+            oldest = person;
+        }
+        console.log(`Oldest: ${JSON.stringify(oldest)} | Person ${JSON.stringify(person)}`);
+        return oldest;
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
