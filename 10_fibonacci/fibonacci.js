364f47c2a8bdfe6f83911bb77ee299f377e50048
Javier Arce <jasearce@espol.edu.ec>
2025-04-06 19:23:48 -0500

    complete fibonacci exercise with for loop and at() function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if ( num < 0 ) return "OOPS";
+    if ( num == 0 ) return 0;
+    if ( num == 1 ) return 1;
+    let fibonacciArr = [];
+    for (let i = 0; i < num; i++) {
+        (i === 0 || i === 1) ? fibonacciArr.push(1) : fibonacciArr.push(fibonacciArr[i-1] + fibonacciArr[i-2]); 
+    }
+    console.log({fibonacciArr});
+    return fibonacciArr.at(num-1);
 };
 
 // Do not edit below this line
