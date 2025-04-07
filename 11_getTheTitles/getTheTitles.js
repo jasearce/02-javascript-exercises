ce2f2407d7fbab29b17cdc7aab6e9d6bc7d6a750
Javier Arce <jasearce@espol.edu.ec>
2025-04-06 19:29:36 -0500

    complete improved version of solution using map() function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,10 +1,16 @@
 const getTheTitles = function(books) {
+    /*
     //Version 1
     let booksTitle = [];
     for (let i = 0; i < books.length; i++) {
         booksTitle.push(books[i].title);
     }
     return booksTitle;
+
+    */
+   
+    //Version 2
+    return books.map(book => book.title);
 };
 
 // Do not edit below this line
